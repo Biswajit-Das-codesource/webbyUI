23fb25efad96ca15c00273d77d217b27d9ac1e13
biswajit das <biswajit9348das@gmail.com>
2025-03-08 19:45:07 +0530

    ok

--- src/components/Thread.jsx
+++ src/components/Thread.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Threads from "../blocks/Backgrounds/Threads/Threads";
 function Thread() {
   return (
-    <div className="bg-black">
+    <div className="bg-black hidden md:block">
       <div style={{ width: "100%", height: "400px", position: "relative" }}>
         <Threads amplitude={2} distance={0} enableMouseInteraction={true} />
       </div>
